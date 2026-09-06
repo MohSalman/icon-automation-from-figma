8627ca56780275fc2b0953377e5eecc29377aa72
MohSalman <30251938+MohSalman@users.noreply.github.com>
2026-09-06 10:27:54 +0000

    feat(icons): sync generated icons from Figma

---
+++ src/icons/Document.tsx
@@ -0,0 +1,9 @@
+import type { IconProps } from '../Icon';
+import { DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR } from '../Icon';
+export function IconDocument({
+  size = DEFAULT_ICON_SIZE,
+  color = DEFAULT_ICON_COLOR,
+  ...props
+}: IconProps) {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" width={size} height={size} {...props}><g fill={color}><path d="M15 22.75H9c-5.43 0-7.75-2.32-7.75-7.75V9c0-5.43 2.32-7.75 7.75-7.75h6c5.43 0 7.75 2.32 7.75 7.75v6c0 5.43-2.32 7.75-7.75 7.75m-6-20C4.39 2.75 2.75 4.39 2.75 9v6c0 4.61 1.64 6.25 6.25 6.25h6c4.61 0 6.25-1.64 6.25-6.25V9c0-4.61-1.64-6.25-6.25-6.25z" /><path d="M15.75 9.75h-7.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h7.5c.41 0 .75.34.75.75s-.34.75-.75.75M15.75 15.75h-7.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h7.5c.41 0 .75.34.75.75s-.34.75-.75.75" /></g></svg>;
+}
